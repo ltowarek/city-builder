12ed3d8030b5add6f31309f31ae9161ff5c011f1
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-09-17 17:43:19 +0200

    Add trivial movement script

---
+++ Assets/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    [SerializeField]
+    private Transform target = null;
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
