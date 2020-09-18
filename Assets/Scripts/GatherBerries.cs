1aa75a8bf92971f8612515e247e50601c5e2f053
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-09-18 16:26:48 +0200

    Add simple AI script

---
+++ Assets/Scripts/GatherBerries.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class GatherBerries : MonoBehaviour
+{
+    [SerializeField]
+    Transform berriesTransform = null;
+
+    [SerializeField]
+    Transform storageTransform = null;
+
+    Transform target = null;
+
+    IAstarAI ai = null;
+
+    void Awake()
+    {
+        target = berriesTransform;
+
+        ai = GetComponent<IAstarAI>();
+        ai.destination = target.position;
+    }
+
+    void Update()
+    {
+        if (ai.reachedDestination)
+        {
+            if (target == berriesTransform)
+            {
+                target = storageTransform;
+            } else
+            {
+                target = berriesTransform;
+            }
+            ai.destination = target.position;
+        }
+    }
+}
