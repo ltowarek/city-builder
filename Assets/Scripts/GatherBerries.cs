71022957343c5ececb5c5939880fbfe46bcc3aa8
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-09-20 22:32:38 +0200

    Implement basic state machine

--- Assets/Scripts/GatherBerries.cs
+++ Assets/Scripts/GatherBerries.cs
@@ -7,34 +7,83 @@ public class GatherBerries : MonoBehaviour
 {
     [SerializeField]
     Transform berriesTransform = null;
-
     [SerializeField]
     Transform storageTransform = null;
-
     Transform target = null;
-
     IAstarAI ai = null;
+
+    enum State
+    {
+        Idle,
+        MovingToResourceNode,
+        GatheringResources,
+        MovingToStorage,
+    }
+    State state = State.Idle;
+
+    int inventory = 0;
+
+    IEnumerator coroutine = null;
 
     void Awake()
     {
-        target = berriesTransform;
-
         ai = GetComponent<IAstarAI>();
-        ai.destination = target.position;
     }
 
     void Update()
     {
-        if (ai.reachedDestination)
+        switch (state)
         {
-            if (target == berriesTransform)
-            {
-                target = storageTransform;
-            } else
-            {
-                target = berriesTransform;
-            }
-            ai.destination = target.position;
+            case State.Idle:
+                target = null;
+                state = State.MovingToResourceNode;
+                break;
+            case State.MovingToResourceNode:
+                if (target == null)
+                {
+                    target = berriesTransform;
+                    ai.destination = target.position;
+                }
+                if (ai.reachedDestination)
+                {
+                    state = State.GatheringResources;
+                }
+                break;
+            case State.GatheringResources:
+                target = null;
+                if (coroutine == null)
+                {
+                    coroutine = WaitAndGatherBerries();
+                    StartCoroutine(coroutine);
+                }
+                if (inventory >= 3)
+                {
+                    StopCoroutine(coroutine);
+                    coroutine = null;
+                    state = State.MovingToStorage;
+                }
+                break;
+            case State.MovingToStorage:
+                if (target == null)
+                {
+                    target = storageTransform;
+                    ai.destination = target.position;
+                }
+                if (ai.reachedDestination)
+                {
+                    inventory = 0;
+                    state = State.Idle;
+                }
+                break;
         }
+    }
+
+    IEnumerator WaitAndGatherBerries()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            inventory++;
+        }   
     }
 }
